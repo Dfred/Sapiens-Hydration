8be5d2dc3462fba1eb73d0420e63ef5cf553bf70
Frederic Delaunay <frederic.delaunay@manymakers.net>
2023-07-24 20:26:16 +0200

    MOD: Hydration mod declaration

--- modInfo.lua
+++ modInfo.lua
@@ -1,12 +1,12 @@
+-- Made with the Sapiens Web Builder
 local modInfo = {
-    name = "MOD_NAME",
-    description = "MOD_DESCRIPTION",
-    preview = "preview.png",
-    version = "0.0.0",
-    type = "MOD_TYPE",
-    developer = "MOD_DEVELOPER",
-    website = "MOD_WEBSITE",
+	name = "Hydration",
+	description = "This mod requires the Hammerstone Framework in order to function. Adds water harvest, storage and consumption to survival.",
+	version = "0.1.0",
+	type = "world",
+	developer = "Dr Robot",
+	loadOrder = 1,
+	preview = "preview.png"
 }
 
-return modInfo 
-
+return modInfo
